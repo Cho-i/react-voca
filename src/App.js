2ae5c585a45392ea91f1d475d474b6744c9a8900
이초이 <choi1223@hunet.co.kr>
2022-09-07 17:48:55 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import Day from './component/Day';
-import DayList from './component/DayList';
-import Header from './component/Header';
-import { Routes, Route } from 'react-router-dom';
-import EmptyPage from './component/EmptyPage';
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import { Routes, Route } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
-	return (
-		<div className="App">
-			<Header />
-			<Routes>
-				<Route path="/" element={<DayList />} />
-				<Route path="/day/:day" element={<Day />} />
-				<Route path="*" element={<EmptyPage />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<DayList />} />
+        <Route path="/day/:day" element={<Day />} />
+        <Route path="*" element={<EmptyPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/component/Day.js
+++ src/component/Day.js
@@ -1,34 +1,31 @@
-import dummy from '../db/data.json'
-import { useParams } from 'react-router-dom';
+import dummy from "../db/data.json";
+import { useParams } from "react-router-dom";
 
 function Day() {
-	const { day } = useParams();
-	const wordList = dummy.words.filter(word => word.day === Number(day));
+  const { day } = useParams();
+  const wordList = dummy.words.filter((word) => word.day === Number(day));
 
-	// 프로퍼티 키를 기준으로 디스트럭처링 할당이 이루어진다. 순서는 의미가 없다.
-	// 변수 lastName, firstName가 선언되고 obj(initializer(초기화자))가 Destructuring(비구조화, 파괴)되어 할당된다.
-	const obj = { firstName: 'Ungmo', lastName: 'Lee' };
-	const { lastName, firstName } = obj;
-	console.log(firstName, lastName);
+  // 프로퍼티 키를 기준으로 디스트럭처링 할당이 이루어진다. 순서는 의미가 없다.
+  // 변수 lastName, firstName가 선언되고 obj(initializer(초기화자))가 Destructuring(비구조화, 파괴)되어 할당된다.
+  const obj = { firstName: "Ungmo", lastName: "Lee" };
+  const { lastName, firstName } = obj;
+  console.log(firstName, lastName);
 
-	return (
-		<>
-			<h2>Day {day}</h2>
-			<table>
-				<tbody>
-					{
-						wordList.map(word => (
-							<tr key={word.id}>
-								<td>{word.eng}</td>
-								<td>{word.kor}</td>
-							</tr>
-						))
-					}
-				</tbody>
-			</table>
-		</>
-
-	)
+  return (
+    <>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {wordList.map((word) => (
+            <tr key={word.id}>
+              <td>{word.eng}</td>
+              <td>{word.kor}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Day;
